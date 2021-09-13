d769ad65d61bcba84c3dfd42d9b79d405d7b0263
piyushgupta09 <32638684+piyushgupta09@users.noreply.github.com>
2021-09-13 11:33:52 +0530

    Create success.blade.php

---
+++ resources/views/success.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div>{{ $reaponse }}</div>
+    <br>
+
+    <div>{{ $res_info_1 }}</div>
+    <br>
+
+    <div>{{ $res_info_2 }}</div>
+    <br>
+
+@endsection
